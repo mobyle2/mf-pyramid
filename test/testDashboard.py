a2ee5657c3047c66fc3a7d653db8bc5dcc46a0d1
Olivier Sallou <olivier.sallou@debian.org>
2012-12-18 14:54:17 +0100

    add class instance test

--- test/testDashboard.py
+++ test/testDashboard.py
@@ -7,7 +7,15 @@ from mf.annotation import Annotation
 
 class TestDashboard(unittest.TestCase):
 
+  def setup(self):
+    Dashboard.__klasses = []
+
   def test_add_to_dashboard(self):
     Dashboard.add_dashboard([User])
     #Annotation.dump()
     assert(User in Annotation.klasses())
+
+  def test_instantiate_from_klasses(self):
+    Dashboard.add_dashboard([User])
+    myuser = Annotation.klasses()[0]()
+    assert(isinstance(myuser,User))
