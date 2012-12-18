d089ad857d2ea54ac318e93931421c7613ee8ef9
Olivier Sallou <olivier.sallou@debian.org>
2012-12-18 09:42:25 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MingForm',
+      version='1.0',
+      description='Ming Form and Pyramid Administration Dashboard',
+      author='Olivier Sallou',
+      author_email='olivier.sallou@irisa.fr',
+      py_modules=['mf'],
+     )
