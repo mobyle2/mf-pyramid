54131ddf54f0b6da209e8658f12650167cb64d32
Olivier Sallou <olivier.sallou@irisa.fr>
2013-01-15 15:24:56 +0100

    fix project url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='mf',
       author='Olivier Sallou',
       author_email='olivier.sallou@irisa.fr',
       license='LGPL',
-      url='https://github.com/osallou/mf-pyramid',
+      url='https://github.com/mobyle2/mf-pyramid',
       packages=['mf'],
       install_requires=["mongokit >= 0.8.2"]
      )
