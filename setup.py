27075ed53f134279747fb1afbe70fe0089290e73
Olivier Sallou <olivier.sallou@irisa.fr>
2013-01-23 09:35:32 +0100

    inc version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='mf',
-      version='0.1.9',
+      version='0.1.10',
       description='MongoKit forms generation and Pyramid Administration Dashboard',
       author='Olivier Sallou',
       author_email='olivier.sallou@irisa.fr',
