331ab4b54c63067f18378308fc2a27279b4c9909
Olivier Sallou <olivier.sallou@irisa.fr>
2013-06-07 16:17:38 +0200

    use setuptools to install requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+requires = [
+    'pymongo',
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'mongokit >= 0.8.2'
+    ]
 
 setup(name='mf',
-      version='0.1.22',
+      version='0.1.23',
       description='MongoKit forms generation and Pyramid Administration Dashboard',
       author='Olivier Sallou',
       author_email='olivier.sallou@irisa.fr',
       license='LGPL',
       url='https://github.com/mobyle2/mf-pyramid',
       packages=['mf'],
-      install_requires=["mongokit >= 0.8.2"]
+      install_requires=requires
      )
