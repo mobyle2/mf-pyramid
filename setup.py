209d9c00cc77ff09d5211c4af8dc178d76d74368
Olivier Sallou <olivier.sallou@irisa.fr>
2013-07-30 09:49:12 +0200

    increase release for pip upload

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = [
     ]
 
 setup(name='mf',
-      version='0.1.24',
+      version='0.1.25',
       description='MongoKit forms generation and Pyramid Administration Dashboard',
       author='Olivier Sallou',
       author_email='olivier.sallou@irisa.fr',
