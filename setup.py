cc478e28c5503b98f1839ed93f6936566ce032b8
Olivier Sallou <olivier.sallou@irisa.fr>
2014-04-18 10:01:00 +0200

    fix history

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = [
     ]
 
 setup(name='mf',
-      version='0.1.36',
+      version='0.1.37',
       description='MongoKit forms generation and Pyramid Administration Dashboard',
       author='Olivier Sallou',
       author_email='olivier.sallou@irisa.fr',
